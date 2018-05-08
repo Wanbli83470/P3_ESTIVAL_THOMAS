978bd234cedde470e106848f9878233cb0de3aac
thomas83470 <estival.t@hotmail.com>
2018-05-08 13:23:08 +0200

    création du labyrinthe, placement des personnages

--- game.py
+++ game.py
@@ -1,8 +1,41 @@
 import pygame
-from pygame locals import *
+from pygame.locals import *
+pygame.init()
 
-windows = 
-fond =
-MacGyver =
-Gardien =
-counter =
+#Ouverture fenetre PyGame
+windows = pygame.display.set_mode((520, 160))
+#chargement du fond
+fond = pygame.image.load("structures.png")
+windows.blit(fond, (0,0))
+#chargement MacGiver
+MacGyver = pygame.image.load("MacGyver.png").convert_alpha()
+position_perso = perso.get_rect()
+windows.blit(MacGyver, (0,0))
+#chargement Gardien
+Gardien = pygame.image.load("Gardien.png").convert_alpha()
+windows.blit(Gardien, (0,0))
+#rafraichissement ecran
+pygame.display.flip()
+
+continuer = 1
+
+while continuer:
+
+
+	for event in pygame.event.get():
+
+		if event.type == QUIT:
+			continuer = 0
+
+        if event.type == KEYDOWN:
+        	if event.key == K_LEFT:
+        		position_perso = position_perso.move(-50,0)
+        	if event.key == K_RIGHT:
+        		position_perso = position_perso.move(50,0)
+        	if event.key == K_UP:
+        		position_perso = position_perso.move(0,-50)
+        	if event.key == K_DOWN:
+        		position_perso = position_perso.move(0,50)
+
+
+#autre fonction:
